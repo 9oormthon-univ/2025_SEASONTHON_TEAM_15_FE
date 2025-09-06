a34a47945d5d0078ebb5052a010689991b2c6e89
Jack <devjack050700@gmail.com>
2025-09-07 07:35:04 +0900

    패키지 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app';
 import '../src/index.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+    Component,
+    pageProps,
+}: AppProps) {
     return <Component {...pageProps} />;
 }
